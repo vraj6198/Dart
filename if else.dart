c4099f76181ded06d9dee5f960e04298f9ebd01a
vraj6198 <37153102+vraj6198@users.noreply.github.com>
2019-04-03 11:37:06 +0530

    Create if else.dart

---
+++ if else.dart
@@ -0,0 +1,29 @@
+void main(){
+  
+  // IF AND ELSE STATEMENTS
+  var salary = 50000;
+  
+  if(salary > 5000 )
+    
+    print("true");
+  
+  else
+    print("false");
+  
+  // if else if ladder statment 
+  var marks = 1000;
+  
+  if (marks >= 80 && marks <100){
+    print("grade A+");
+  }
+  else if(marks >= 60 && marks < 79)
+  {
+    print("grade B"); }
+  else if(marks<=40)
+  {
+    print("fail");
+  }else
+  {
+    print("invalid marks");
+  }
+}
