64e728acec2aa3d8b0f00271f33118fc611dadcd
vraj6198 <37153102+vraj6198@users.noreply.github.com>
2019-04-03 11:33:23 +0530

    Create  data type and variable declare.dart

---
+++ data type and variable declare.dart
@@ -0,0 +1,15 @@
+void main(){
+    int age=10;
+    String name="vraj";
+    double percentage=67.666;
+    double hexvalue= 0xEEAABBCCDD;
+    double expo = 1.22e45;
+    bool isvalid = true;
+    
+    print(age);
+    print(name);
+    print(percentage);
+    print(hevalue);
+    print(expo);
+    print(isvalid);
+}
