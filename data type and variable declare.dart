6e82563d75d02063fb8d0a57398c81a2c2a861d8
vraj6198 <37153102+vraj6198@users.noreply.github.com>
2019-04-15 18:40:15 +0530

    Update data type and variable declare.dart

--- data type and variable declare.dart
+++ data type and variable declare.dart
@@ -1,6 +1,6 @@
 void main(){
-    int age=10;
-    String name="vraj";
+    int age=10;     // var age = 10;
+    String name="vraj";     // var name = vraj;
     double percentage=67.666;
     double hexvalue= 0xEEAABBCCDD;
     double expo = 1.22e45;
