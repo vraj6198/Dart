269f6a8c3f89ebd370b1af31823c1196e3f9a44e
patel vraj <37153102+vraj6198@users.noreply.github.com>
2020-03-23 19:16:11 +0530

    Update data type and variable declare.dart

--- data type and variable declare.dart
+++ data type and variable declare.dart
@@ -12,9 +12,13 @@ sets
 maps
 runes (for expressing Unicode characters in a string)
 symbols
+ 
+ Notes: All the Data Types are objects
+  -> Therefore, Values are by default is NULL
 
 every variable in Dart refers to an object—an instance of a class—you can usually use constructors to initialize variables. 
-Some of the built-in types have their own constructors. For example, you can use the Map() constructor to create a map.
+Some of the built-in types have their own constructors. 
+  For example, you can use the Map() constructor to create a map.
 
 void main(){
     int age=10;     // var age = 10;
@@ -23,7 +27,7 @@ void main(){
     double hexvalue= 0xEEAABBCCDD;
     double expo = 1.22e45;
     bool isvalid = true;
-    var number = 10;
+    var number = 10; // it is inferred as interger automatically
     
     print(age);
     print(number.runtimeType); // this will show datatype of the variable
