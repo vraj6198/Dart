dd6698999bf9b8b3a49b9522b6288af55f0162ea
patel vraj <37153102+vraj6198@users.noreply.github.com>
2020-03-24 11:53:33 +0530

    Update loop control stmt.dart

--- loop control stmt.dart
+++ loop control stmt.dart
@@ -1,8 +1,20 @@
 /* Iterator:  
+
+Introduction to Iterator : 
   
-          Loop: definite-> for loop
-               Indefinite -> while loop and do while loop */
+  Loop: Definite -> for loop => (if you know how many times your loop should executed so in that case use For Loop)
+        Indefinite -> while loop and do while loop => (In case if you don't know how many numbers of times you want to execute your code
+		      or in case you only want to execute the code based on some codition then in that case use indefinite loop)		
+		*/
+
 // FOR LOOP
+
+ /* FOR LOOP Syntax
+ 	for(initializer; condition; increment/decrement){
+	// Put your code 
+	} */
+
+// WAP to find even number from 1 to 10
  void main(){
    
    for(int i=0; i<=10; i++)
@@ -20,12 +32,16 @@
  }
 
 // WHILE LOOP
+
 /* WHILE LOOP SYNTAX 
-initializer counter
-while (condition){
-put executes code
-increment and  decrment
-} */
+
+	initializer counter
+	while (condition){
+	put executes code
+	increment and  decrement
+	} */
+
+//WAP to find even number from 1 to 10
 void main(){
   
 	int i = 0;
@@ -37,6 +53,16 @@ void main(){
 }
 
 // DO-WHILE LOOP
+
+/*  DO-WHILE LOOP Syntax
+	
+	Initialize Counter 
+	do{
+	      Put your code here
+	      Increment and decrement
+	      }while(condition); */
+
+//WAP to find even number from 1 to 10
 void main(){
   var i = 0;
   do{
